fe56249b2719a33fc1f98881936fc6a94115e3ce
lihao <1957512314@qq.com>
2017-09-19 07:10:08 +0800

    完善登录登出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,31 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function () {
+  $(this).next().slideToggle();
+});
+// 退出登录时
+$('#logout').click(function () {
+  $.ajax({
+    url: '/api/logout',
+    type: 'post',
+    dataType: 'json',
+    success: function () {
+      location.href = '/main/login'
+    }
+  });
+});
+// 验证用户是否登陆了
+var flag = $.cookie('PHPSESSID');
+if (!flag) {
+  location.href = '/main/login';
+}
+// 填充头像信息
+var loginInfo = $.cookie('loginInfo');
+loginInfo = loginInfo && JSON.parse(loginInfo);
+$('.aside .profile img').attr('src', loginInfo.tc_avatar);
+$('.aside .profile h4').html(loginInfo.tc_name);
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+
+
