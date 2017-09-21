7041692fd75ee56fff558b8a62d19e3a4efda988
lihao <1957512314@qq.com>
2017-09-21 16:45:26 +0800

    完成讲师启用和注销

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,12 +1,37 @@
 define(['jquery', 'template'], function ($, template) {
-  //
+  //讲师列表
   $.ajax({
     url: '/api/teacher',
     type: 'get',
     dataType: 'json',
     success: function (data) {
       var html = template('teacherTpl', {list: data.result});
-      $("#teacherInfo").html(html)
+      $("#teacherInfo").html(html);
+      //启用与注销
+      $('.control').click(function(){
+       //！！！ var dataId= $('.control').parent('td')  这样获取的都是同一个dataId
+        var dataId= $(this).attr('data-id');
+        var dataStatus= $(this).attr('data-status');
+        var that=this;
+       console.log(that)
+        $.ajax({
+          url: '/api/teacher/handle',
+          type: 'post',
+          dataType: 'json',
+          data: {tc_id: dataId,tc_status:dataStatus},
+          success:function (data){
+            if (data.code==200) {
+              //console.log(this)  这里this不是$('.control')，所有上面用that
+              $(that).attr('data-status',data.result.tc_status);
+              if (data.result.tc_status==0) {
+                $(that).text("启用")
+              }else{
+                $(that).text("注销")
+              }
+            }
+          }
+        })       
+       });
     }
   });
 });
