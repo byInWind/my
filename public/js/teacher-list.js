b44ddc08372e378cd85dc619e2db354ed06a2ec3
lihao <1957512314@qq.com>
2017-09-21 17:24:29 +0800

    修改启用与注销

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,30 +8,44 @@ define(['jquery', 'template'], function ($, template) {
       var html = template('teacherTpl', {list: data.result});
       $("#teacherInfo").html(html);
       //启用与注销
-      $('.control').click(function(){
-       //！！！ var dataId= $('.control').parent('td')  这样获取的都是同一个dataId
-        var dataId= $(this).attr('data-id');
-        var dataStatus= $(this).attr('data-status');
-        var that=this;
-       console.log(that)
+      $('.control').click(function () {
+        //！！！ var dataId= $('.control').parent('td')  这样获取的都是同一个dataId
+        var dataId = $(this).parent('td').attr('data-id');
+        var dataStatus = $(this).attr('data-status');
+        var that = this;
         $.ajax({
           url: '/api/teacher/handle',
           type: 'post',
           dataType: 'json',
-          data: {tc_id: dataId,tc_status:dataStatus},
-          success:function (data){
-            if (data.code==200) {
+          data: {tc_id: dataId, tc_status: dataStatus},
+          success: function (data) {
+            if (data.code == 200) {
               //console.log(this)  这里this不是$('.control')，所有上面用that
-              $(that).attr('data-status',data.result.tc_status);
-              if (data.result.tc_status==0) {
+              $(that).attr('data-status', data.result.tc_status);
+              if (data.result.tc_status == 0) {
                 $(that).text("启用")
-              }else{
+              } else {
                 $(that).text("注销")
               }
             }
           }
-        })       
-       });
+        })
+      });
+
+      //查 看
+      //$('.look').click(function () {
+      //  var dataId = $(this).parent('td').attr('data-id');
+      //  var that = this;
+      //  $.ajax({
+      //    url: '/api/teacher/view',
+      //    type: 'get',
+      //    dataType: 'json',
+      //    data: {tc_id: dataId},
+      //    success: function (data) {
+      //      console.log(data)
+      //    }
+      //  });
+      //});
     }
   });
 });
