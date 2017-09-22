4ec2f388b14467d62163091bee30c4393f698ee4
lihao <1957512314@qq.com>
2017-09-22 08:12:50 +0800

    初步完成查看与添加讲师功能，未加时间插件

--- public/js/teacheradd.js
+++ public/js/teacheradd.js
@@ -9,21 +9,32 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
       data: {tc_id: tcId},
       dataType: 'json',
       success: function (data) {
-        data.result.operate='编辑讲师';
+        data.result.operate = '编辑讲师';
         var html = template('teacherTpl', data.result);
         $("#teacherInfo").html(html);
+        submitForm('/api/teacher/update');
       }
     });
   } else {
     //添加操作
-    $.ajax({
-      url: '/api/teacher',
-      type: 'get',
-      dataType: 'json',
-      success: function (data) {
-        var html = template('teacherTpl', {operate:'添加讲师'});
-        $("#teacherInfo").html(html);
-      }
-    });
+    var html = template('teacherTpl', {operate: '添加讲师'});
+    $("#teacherInfo").html(html);
+    submitForm('/api/teacher/add')
   }
+  //提交表单
+  function submitForm(url) {
+    $('#teacherBtn').click(function () {
+      $.ajax({
+        url: url,
+        type: 'post',
+        data: $("#teacherForm").serialize(),
+        dataType: 'json',
+        success: function (data) {
+          if (data.code == 200) {
+            location.href = '/teacher/list';
+          }
+        }
+      });
+    });
+  };
 });
