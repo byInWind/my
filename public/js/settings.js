3fbeda42080707cd205c62bb96fa28a68ef1750e
lihao <1957512314@qq.com>
2017-09-24 17:46:55 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR) {
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language','validate','form'],function($,template,CKEDITOR) {
 	//ckeditor顺序。在第三个，参数里应该也排在第三，否则找不到
   $.ajax({
 		url: '/api/teacher/profile',
@@ -28,6 +28,49 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
             });
              //富文本
              CKEDITOR.replace('edit');
+             //提交表单
+             $("#settingsForm").validate({
+              //关闭默认提交 按钮应是submit
+              sendForm : false,
+              valid : function (){
+                var hometown='';
+                var p=$("#p").find('option:selected').text();
+                var c=$("#c").find('option:selected').text();
+                var d=$("#d").find('option:selected').text();
+                hometown = p+'-'+c+'-'+d;
+         
+                //富文本无法更改，需要用ckeditor的方法更新富文本内容
+                for(var instance in CKEDITOR.instances){
+                  CKEDITOR.instances[instance].updateElement();
+                }
+              //提交表单
+                $(this).ajaxSubmit({
+                  url: '/api/teacher/modify',
+                  type: 'post',
+                  dataType: 'JSON',
+                  data: {tc_hometown :hometown},
+                  success : function (data){
+                     if(data.code == 200){
+                         // 刷新当前页面
+                      location.reload();
+              }
+                 }
+                })     
+           //直接ajax500
+              //  $(this).ajaxSubmit({
+              //     url: '/api/teacher/modify',
+              //     type: 'post',
+              //     dataType: 'JSON',
+              //     data: {tc_hometown :hometown},
+              //     success : function (data){
+              //        if(data.code == 200){
+              //            // 刷新当前页面
+              //         location.reload();
+              // }
+              //    }
+              //   })           
+              }
+             });
 		}
 	});
 	
