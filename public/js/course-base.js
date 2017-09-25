8ab25799334e6b503d28c1051eed853f3741739f
lihao <1957512314@qq.com>
2017-09-25 14:57:55 +0800

    完成课程基本信息填充

--- public/js/course-base.js
+++ public/js/course-base.js
@@ -8,17 +8,39 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
     url: '/api//course/basic',
     data: {cs_id: csId},
     type: 'get',
-    datatyp: 'json',
+    dataType: 'json',
     success: function (data) {
-
       if (flag) {
-      data.result.operate='编辑课程';
+        data.result.operate = '编辑课程';
       }
       else {
-        data.result.operate='添加课程';
+        data.result.operate = '添加课程';
       }
       var html = template('baseTpl', data.result);
       $("#baseInfo").html(html);
+      //处理二级分类的下拉联动操作
+      $("#firstType").change(function () {
+        var cgId = $(this).val();
+        //??cgId不存在  base落下了$.value
+       // console.log($(this))
+
+        //获取一级分类id
+        $.ajax({
+          url: '/api/category/child',
+          type: 'get',
+          dataType: 'json',
+          data: {cg_id: cgId},
+          success: function (data) {
+            var tpl = ' <option>请选择分类</option>' +
+              '{{each list}}' +
+              '<option value="{{$value.cg_id}}">{{$value.cg_name}}</option> ' +
+              '{{/each}}';
+            var html = template.render(tpl, {list: data.result});
+            console.log(html)
+            $("#secondType").html(html);
+          }
+        })
+      })
     }
   })
 });
