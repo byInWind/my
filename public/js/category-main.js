e46d18cfc88f5021f7cab4fbd3ab48e7885a4693
lihao <1957512314@qq.com>
2017-09-27 22:29:35 +0800

    完善课程分类界面

--- public/js/category-main.js
+++ public/js/category-main.js
@@ -1,19 +1,15 @@
-/**
- * 分类列表模块
- */
-
 define(['jquery', 'util', 'template','state'], function ($, util, template) {
   // 设置导航
-  util.setSelect('/category/list');
+  util.setSelect(location.pathname);
 
   // 查看所有分类
   $.ajax({
     url: '/api/category',
-    type: 'post',
-    success: function (info) {
-      if(info.code == 200) {
+    type: 'get',
+    success: function (data) {
+      if(data.code == 200) {
         // 模板引擎
-        var html = template('categoryTpl', {list: info.result});
+        var html = template('categoryTpl', {list: data.result});
         // 添加DOM
         $('#categoryInfo').html(html);
       }
