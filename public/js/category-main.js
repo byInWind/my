6945d8174b0cb5524eb2cc0247d425b3f31a2eb8
lihao <1957512314@qq.com>
2017-09-27 21:37:41 +0800

    完成分类界面渲染

--- public/js/category-main.js
+++ public/js/category-main.js
@@ -1,16 +1,22 @@
-define(['jquery', 'template', 'util', 'state'], function ($, template, util) {
-  //左侧导航高亮
-  util.setSelect(location.pathname)
-  //获取数据
+/**
+ * 分类列表模块
+ */
+
+define(['jquery', 'util', 'template','state'], function ($, util, template) {
+  // 设置导航
+  util.setSelect('/category/list');
+
+  // 查看所有分类
   $.ajax({
-    url: '/api/category/top',
-    type: 'GET',
-    dataType: 'json',
-    success: function (data) {
-      if (data.code==200) {
-        var html = template('categoryTpl', {list: data.result});
-        $("#categoryInfo").html(html);
+    url: '/api/category',
+    type: 'post',
+    success: function (info) {
+      if(info.code == 200) {
+        // 模板引擎
+        var html = template('categoryTpl', {list: info.result});
+        // 添加DOM
+        $('#categoryInfo').html(html);
       }
     }
-  })
+  });
 });
