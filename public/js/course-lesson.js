1858b26ecc99a911a4faa9c072e3e581ca7208cb
lihao <1957512314@qq.com>
2017-09-27 17:03:36 +0800

    完成课时添加和编辑的表单提交

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,7 +1,8 @@
-define(['jquery', 'template', 'util', 'bootstrap'], function ($, template, util) {
+define(['jquery', 'template', 'util', 'bootstrap', 'form'], function ($, template, util) {
 
   //设置导航高亮
   util.setSelect('/course/add');
+  //获取课程ID
   var csId = util.all('cs_id');
   //响应
   $.ajax({
@@ -14,25 +15,40 @@ define(['jquery', 'template', 'util', 'bootstrap'], function ($, template, util)
         //渲染模板
         var html = template('lessonTpl', data.result);
         $("#lessonInfo").html(html);
-         //添加课程
+        //添加课程
         $("#addLesson").click(function () {
           //添加时内容为空,{}
           var html = template('modalTpl', {});
           $("#chapterModal").html(html);
           //弹出模态框
           $("#chapterModal").modal();
+          //添加课时信息表单提交
+          $("#editBtn").click(function () {
+            $("#lessonForm").ajaxSubmit({
+              url: '/api/course/chapter/add',
+              type: 'post',
+              dataType: 'json',
+              data : {ct_cs_id : csId},
+              success: function (data) {
+                if(data.code == 200){
+                 // console.log(data)
+                  location.reload();
+                }
+              }
+            })
+          })
         });
 
         //编辑课程
         $(".editLesson").click(function () {
-          var ctId = $(this).attr('data-ctId');
+          var ctId = $(".editLesson").attr('data-ctId');
           $.ajax({
             url: '/api/course/chapter/edit',
             type: 'GET',
             dataType: 'json',
             data: {ct_id: ctId},
             success: function (data) {
-             // console.log(data)
+              // console.log(data)
               if (data.code == 200) {
                 //  //渲染内容
                 var html = template('modalTpl', data.result);
@@ -40,6 +56,21 @@ define(['jquery', 'template', 'util', 'bootstrap'], function ($, template, util)
                 $(".modal-title").html('编辑课程')
                 //  //弹出模态框
                 $("#chapterModal").modal();
+                //在这儿修改课程，表单提交
+                $("#editBtn").click(function () {
+                  $("#lessonForm").ajaxSubmit({
+                    url: '/api/course/chapter/modify',
+                    type: 'post',
+                    dataType: 'json',
+                    data: {ct_cs_id : csId,ct_id : ctId},
+                    success: function (data) {
+                      if(data.code == 200){
+                        //alert(ctId)
+                        location.reload();
+                      }
+                    }
+                  });
+                })
               }
             }
           });
